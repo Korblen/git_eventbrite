6909eff81892579c958fdfa82b4d8403fdf84c26
Korblen <malo.bastianelli@gmail.com>
2024-02-21 00:08:36 +0100

    sans le env

--- app/mailers/event_mailer.rb
+++ app/mailers/event_mailer.rb
@@ -1,3 +1,12 @@
 class EventMailer < ApplicationMailer
     default from: 'notifications@votredomaine.com'
+    def event_created(event)
+        @event = event
+        mail(to: @event.user.email, subject: 'Votre événement a été créé !')
+    end
+    def event_participation(user, event)
+        @user = user
+        @event = event
+        mail(to: @event.user.email, subject: 'Nouvelle participation à votre événement !')
+    end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -6,15 +6,7 @@ class Event < ApplicationRecord
     validates :description, length: { in: 20..1000 }
     validates :price, numericality: { only_integer: true, greater_than: 0, less_than_or_equal_to: 1000 }
     validates :location, presence: true
-    def event_created(event)
-        @event = event
-        mail(to: @event.admin.email, subject: 'Votre événement a été créé avec succès !')
-    end
-    def event_participation(user, event)
-        @user = user
-        @event = event
-        mail(to: @event.admin.email, subject: 'Nouvelle participation à votre événement !')
-    end
+
     private
 
     def send_creation_email
